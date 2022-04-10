1a727dc83fe0f74adfa93cb93f187532f7577374
cidja <chgeorgesc@gmail.com>
2022-04-10 22:02:46 +0200

    first commit

---
+++ infoserveur.php
@@ -0,0 +1,4 @@
+<h2>Voici les infos du serveur </h2>
+<?php
+echo phpinfo();
+?>
---
+++ one.php
@@ -0,0 +1,22 @@
+<h1>C'est ici que je commence</h1>
+<?php
+echo "je vais essayer d'afficher quelque chose<br/>";
+$a = 'je suis une chaine de caractères<br/>';
+echo "avec var_dump la variable a <br/>";
+echo var_dump($a);
+?>
+
+<h2>Les tableaux</h2>
+
+<?php
+
+$b = [1,2,3,5];
+foreach($b as $valeurs){
+    echo "je suis $valeurs";
+}
+
+$c = [ 'prenom' => 'jean', 'nom' => 'Marcel', 'profession' => 'charpentier'];
+/* foreach($c as $key=>$value){
+    echo $key+ ' = '+ $value +'<br/>';
+} */
+var_dump($c);
